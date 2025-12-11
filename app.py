95197a1a546cf1b38ea546d47e97c3237432a005
Piratebird <allenandersonuwu@gmail.com>
2025-12-11 03:10:56 +0200

    some import and configs

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-# the main entry of the program/site
-from flask import Flask, render_template
+# !!the main entry of the program/site !! #
+
+# importing the session module and redirect and other needed utilities
+from flask import Flask, render_template, request, session, redirect, url_for
+
+# importing session from flask_session to manage user_session
+from flask_session import Session
+
+# importing the database from the /core directory and assign it to db
+from core import db_management as db
 
 app = Flask(__name__)
+
+# ---- SESSION CONFIG -------#
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.secret_key = "TOONIX8081"
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_PERMANENT"] = False
 
 
 @app.route("/")
