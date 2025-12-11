674377e69703b27d5bf24c09c045fa7707891bf6
Piratebird <allenandersonuwu@gmail.com>
2025-12-11 02:31:40 +0200

    fixing the multiple routing issue

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/","/home")
+@app.route("/")
+@app.route("/home")
 def home_page():
     return render_template("home.html")
 
-@app.route('/signup')
+
+@app.route("/signup")
+def signup():
+    pass
+
 
 if __name__ == "__main__":
     app.run(debug=True)
