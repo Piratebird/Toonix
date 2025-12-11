70267ce894199be496e2fccedb6a960fe3e57b58
Piratebird <allenandersonuwu@gmail.com>
2025-12-11 02:27:47 +0200

    small changes and naming tweaks

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def hero_section():
+@app.route("/","/home")
+def home_page():
     return render_template("home.html")
 
+@app.route('/signup')
 
 if __name__ == "__main__":
     app.run(debug=True)
