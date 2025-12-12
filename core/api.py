d88729e2e34dc37d0763e3e7c6f5143d3f7a1c78
Piratebird <allenandersonuwu@gmail.com>
2025-12-12 02:12:53 +0200

    again fix syntax error remove s

--- core/api.py
+++ core/api.py
@@ -15,6 +15,6 @@ CHAPTER_CAHCE_DIR = "data/cache/chapters"
 DOWNLOADS_DIR = "data/downloads"
 
 # create needed direcctories
-os.makedirs(CACHE_DIR, exit_ok=True)
-os.makedirs(CHAPTER_CAHCE_DIR, exit_ok=True)
-os.makedirs(DOWNLOADS_DIR, exit_ok=True)
+os.makedirs(CACHE_DIR, exist_ok=True)
+os.makedirs(CHAPTER_CAHCE_DIR, exist_ok=True)
+os.makedirs(DOWNLOADS_DIR, exist_ok=True)
